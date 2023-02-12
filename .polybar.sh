771818996a32424f9607603030b41379a35b1696
fluffybunny <fllfybnny@gmail.com>
2023-02-12 16:33:11 +0100

    using hard links

--- .polybar.sh
+++ .polybar.sh
@@ -1,1 +1,20 @@
-/home/fluffybunny/.polybar.sh
+#!/usr/bin/env sh
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -x polybar >/dev/null; do sleep 1; done
+
+screens=$(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f6)
+
+if [[ $(xrandr --listactivemonitors | grep -v "Monitors" | cut -d" " -f4 | cut -d"+" -f2- | uniq | wc -l) == 1 ]]; then
+  MONITOR=$(polybar --list-monitors | cut -d":" -f1) TRAY_POS=right polybar main &
+else
+  primary=$(xrandr --query | grep primary | cut -d" " -f1)
+
+  for m in $screens; do
+      echo m
+      MONITOR=$m TRAY_POS=right polybar main &
+  done
+fi
