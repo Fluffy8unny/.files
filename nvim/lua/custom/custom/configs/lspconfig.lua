be31622fa7f92f5a1aea07a54dbbd2a214aeb888
Fluffybunny <Fluffybunny>
2025-01-18 11:20:51 +0100

    customs folder was in ignore for some reason

---
+++ nvim/lua/custom/custom/configs/lspconfig.lua
@@ -0,0 +1,35 @@
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+
+local lspconfig = require("lspconfig")
+
+lspconfig.clangd.setup{
+  on_attach = function (client,bufnr)
+    on_attach(client,bufnr)
+  end,
+  capabilities = capabilities,
+}
+lspconfig.hls.setup{}
+
+lspconfig.rust_analyzer.setup({
+    on_attach = on_attach,
+    settings = {
+        ["rust-analyzer"] = {
+            imports = {
+                granularity = {
+                    group = "module",
+                },
+                prefix = "self",
+            },
+            cargo = {
+                buildScripts = {
+                    enable = true,
+                },
+            },
+            procMacro = {
+                enable = true
+            },
+        }
+    }
+})
---
+++ nvim/lua/custom/plugins.lua
@@ -0,0 +1,28 @@
+local plugins={
+  {
+    "neovim/nvim-lspconfig",
+    config = function ()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "clangd",
+        "clang-format",
+        "codelldb",
+      }
+    }
+  },
+}
+
+return plugins
